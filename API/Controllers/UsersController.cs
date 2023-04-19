d1d97a6ec102f8612f82d1b95c1b64e5aba8b826
Pawanyy <pawanyyy01@gmail.com>
2023-04-20 03:36:19 +0530

    added async method in controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
+using System.Collections;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -12,6 +15,19 @@ namespace API.Controllers
         public UsersController(DataContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser([FromRoute] int id){
+            return await _context.Users.FindAsync(id);
         }
     }
 }
