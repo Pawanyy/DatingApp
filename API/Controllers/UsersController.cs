32ed2c68d9e4e3299fb1103627be67bb5d90a932
Pawanyy <pawanyyy01@gmail.com>
2023-04-28 03:35:56 +0530

    added authorize on users end points

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,6 +17,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = await _context.Users.ToListAsync();
@@ -24,6 +26,7 @@ namespace API.Controllers
 
         [HttpGet]
         [Route("{id:int}")]
+        [Authorize]
         public async Task<ActionResult<AppUser>> GetUser([FromRoute] int id){
             return await _context.Users.FindAsync(id);
         }
