c2085808fabefc964b5e1da54d6f16b0e214259f
Pawanyy <pawanyyy01@gmail.com>
2023-04-30 02:52:47 +0530

    fixed with async pipe for service unsuscribe

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/User';
 
 @Component({
   selector: 'app-nav',
@@ -9,27 +11,17 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  loggedIn: boolean = false;
 
-  constructor(private accountService: AccountService) {
+  constructor(public accountService: AccountService) {
   }
 
   ngOnInit(): void {
-    this.getCurrentUser()
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: user => this.loggedIn = !!user,
-      error: error => console.log(error)
-    })
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response)
-        this.loggedIn = true;
       },
       error: err => console.log(err),
       complete: () => console.log('Login Successfully!')
