02dd472d2cb43256d5ddecb391eb6e7dac95f7a4
Pawanyy <pawanyyy01@gmail.com>
2023-04-30 03:29:48 +0530

    transfered getusers from appcomp o home component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/User';
 import { AccountService } from './_services/account.service';
@@ -11,21 +10,11 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit{
 
   title: string = 'client';
-  users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:7208/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Resquest has Completed!')
-    })
   }
 
   setCurrentUser() {
