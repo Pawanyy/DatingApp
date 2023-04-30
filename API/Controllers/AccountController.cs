c4c145afe175610513ea9aa38109f2a7648e78a9
Pawanyy <pawanyyy01@gmail.com>
2023-05-01 02:57:38 +0530

    added from body tag

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ namespace API.Controllers
         }
 
         [HttpPost("register")] //POST: api/account/register
-        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto>> Register([FromBody] RegisterDto registerDto)
         {
             if(await UserExist(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -42,7 +42,7 @@ namespace API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login([FromBody] LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
