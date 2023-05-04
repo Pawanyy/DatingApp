20bfc573099344bc39e1384b695ca73561bcfcf7
Pawanyy <pawanyyy01@gmail.com>
2023-05-04 23:41:30 +0530

    added relationship and extended userEntity

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities
 {
+    [Table("Photos")]
     public class Photo
     {
         public int Id { get; set; }
         public int Url { get; set; }
         public int IsMain { get; set; }
         public int PublicMain { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
